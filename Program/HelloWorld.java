e64f1a73d41e5d8b37ff672e43b3ecfc1e5ac7fa
NARMATHA1 <110030648+NARMATHA1@users.noreply.github.com>
2022-07-28 16:11:40 +0530

    Add files via upload

---
+++ Program/HelloWorld.java
@@ -0,0 +1,13 @@
+/*@Author:Narmatha L
+Date:28/07/2022
+Title:HelloWorld Program
+Question:Executing Basic Java Program
+*/
+class HelloWorld
+{
+	//Calling main method
+	public static void main (String[]args)
+	{
+	System.out.println("Hello I am JAVA");
+	}
+}
